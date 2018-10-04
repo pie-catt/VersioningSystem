99b220f7bfc1b6485b26b74f206576058d80964e
Pietro Cattaneo <pieca314@gmail.com>
2018-10-04 11:44:50 +0200

    Secondo Commit su Beta

--- MyVersion/Program.cs
+++ MyVersion/Program.cs
@@ -14,6 +14,7 @@ namespace MyVersion
             Console.ReadLine();
             Console.WriteLine("Ramo Master");
             Console.WriteLine("Ora ramo Beta");
+            Console.WriteLine("Secondo commit su Beta");
         }
     }
 }
